64bb0e1827d81088939fc48b6b3eede6729db34c
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-04-08 01:41:51 +0300

    Add project files.

---
+++ AspNetMVC_Introduction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMVC_Introduction.Startup))]
+namespace AspNetMVC_Introduction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
